2cf8ccc1eaafaa83e657a960a9c16678e23e1d49
Marcello Romani <lello107@hotmail.com>
2016-11-29 13:23:08 +0100

    date time func

--- lib/timecode.rb
+++ lib/timecode.rb
@@ -30,7 +30,9 @@ def self.convert_to_frames(timecode)
         return frames
 
 
-    end
+end
+
+
 
  def self.convert_from_frames(frames)
 
@@ -64,6 +66,46 @@ def self.convert_to_frames(timecode)
         timecode = addzero(intHours.to_s)  + ":" + addzero(intMins.to_s) + ":" + addzero(intSecs.to_s) + ":" + addzero(intFrames.to_s)
         return timecode
     end
+
+
+
+ def self.convert_from_frames_to_datetime(frames)
+
+        intHours, intMins, intSecs, intFrames, intTmp=0
+        timecode =""
+
+        #Convert frames to hours, minutes and seconds.
+
+        #Total number of seconds
+        intSecs = (frames / 25).to_i
+
+        #Total number of minutes
+        intMins = (intSecs / 60).to_i
+
+        #Total number of hours
+        intHours = (intSecs / 3600).to_i
+
+        #Number of secs remaining after subtracting  number of mins.
+        intSecs = ((frames / 25) - (intMins * 60)).to_i
+
+        #Number of mins remaining after subtracting number of hours.
+        intMins = intMins - (intHours * 60)
+
+        #Determine the number of frames remaining after subtracting hours,mins and secs
+        intTmp = intSecs * 25
+        intTmp = intTmp + (intMins * 60 * 25)
+        intTmp = intTmp + (intHours * 60 * 60 * 25)
+        intFrames = frames - intTmp
+
+        if(intFrames==25)
+            intFrames=24
+        end
+
+        #Convert to string, adding leading 0 where value is less than 10,and separating with ':'
+        timecode = addzero(intHours.to_s)  + ":" + addzero(intMins.to_s) + ":" + addzero(intSecs.to_s) + "." + "%03d" % (intFrames*40)
+        return timecode
+    end
+
 
   def self.add_timecode(timecode1, timecode2)
         tc1 ,tc2 = 0
